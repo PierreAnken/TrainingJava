70b779648ff8330c8435cddd550d4d137f5db79b
Pierre Anken <pierre.anken@pm.me>
2022-01-15 14:39:18 +0100

    cleanup

--- Exercices/General/Bases/src/Graphs/GraphMethodPondere.java
+++
@@ -1,7 +0,0 @@
-package Graphs;
-
-public interface GraphMethodPondere extends GraphMethod{
-
-    void addRelation(Node source, Node dest, int cost);
-    int getTravelCost(Node source, Node dest);
-}
--- Exercices/General/Bases/src/Graphs/Relation.java
+++ Exercices/General/Bases/src/Graphs/Relation.java
@@ -3,19 +3,12 @@ package Graphs;
 
 public class Relation {
 
-    private Node source;
-    private Node dest;
-    private int cost;
+    private final Node source;
+    private final Node dest;
 
     public Relation (Node source, Node dest){
         this.source = source;
         this.dest = dest;
-    }
-
-    public Relation (Node source, Node dest, int cost){
-        this.source = source;
-        this.dest = dest;
-        this.cost = cost;
     }
 
     public Node getSource() {
@@ -26,14 +19,10 @@ public class Relation {
         return dest;
     }
 
-    public int getCost() {
-        return cost;
-    }
-
     @Override
     public boolean equals(Object o) {
-        if (!(o instanceof Relation)) return false;
-        Relation relation = (Relation) o;
+        if (!(o instanceof Relation relation)) return false;
+
         return source.getName() == relation.source.getName()
                 && dest.getName() == relation.dest.getName();
     }
